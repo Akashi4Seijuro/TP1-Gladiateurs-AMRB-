f81852ee64a0633f95b93937e2a791f885566968
202230076 <202230076@PO-24861>
2024-09-13 15:11:54 -0400

    quatrième étape achevée avec succès

--- JeuGladiateurs/src/combat/CompteurDeTour.java
+++ JeuGladiateurs/src/combat/CompteurDeTour.java
@@ -6,7 +6,8 @@ public class CompteurDeTour {
     // **************************************************************************
     // **************************************************************************
     // <editor-fold defaultstate="collapsed" desc="Attributs">
-    // TODO : Mettre vos attributs ici
+    int cptrTour;
+    
     // </editor-fold>
     
     // **************************************************************************
@@ -14,7 +15,7 @@ public class CompteurDeTour {
     // **************************************************************************
     // <editor-fold defaultstate="collapsed" desc="Constructeurs et initialisation">
     public CompteurDeTour() {
-        // TODO : Constructeur SANS paramètres qui initialise un compteur de tour (attribut) à 1
+    this.cptrTour = 1;
     }
     // </editor-fold>
 
@@ -22,20 +23,25 @@ public class CompteurDeTour {
     // **************************************************************************
     // **************************************************************************
     // <editor-fold defaultstate="collapsed" desc="Getters et setters">
-    // TODO : Le getter
-    // TODO : Le setter
-    // </editor-fold>
+    public int getCptrTour() {
+        return cptrTour;
+    }
 
+    public void setCptrTour(int cptrTour) {
+        this.cptrTour = cptrTour;
+    }
+    
+    // </editor-fold>
     // **************************************************************************
     // **************************************************************************
     // **************************************************************************
     // <editor-fold defaultstate="collapsed" desc="Mécanique de jeu">
     public void augmenteTour() {
-        // TODO : Incrémenter le compteur de tour
+        this.cptrTour += 1;
     }
 
     public void afficheTour() {
-        // TODO : Afficher le tour actuel, tel que montré dans l'énoncé
-    }
+   System.out.println("---------------\nTOUR ACTUEL: " +this.getCptrTour() +"\n---------------");
+           }
     // </editor-fold>
 }
